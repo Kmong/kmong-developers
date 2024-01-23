f335a3b33881412b817e598c30e202219f41209a
uu29 <emma@kmong.com>
2024-01-23 21:16:54 +0900

    feat: sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import packageInfo from './package.json';
 
 Sentry.init({
   dsn: "https://78244d347b94fbc2be076209148d8ab4@o4506552386715648.ingest.sentry.io/4506552392417280",
@@ -11,20 +12,44 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
+  debug: true,
+  environment: process.env.NODE_ENV,
+  release: packageInfo.version,
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
+  ignoreErrors: ['밀크티'],
+  denyUrls: ['sentry-example-page'],
+  // allowUrls: ['sentry-example-page'],
+  autoSessionTracking: true,
+  beforeBreadcrumb(breadcrumb, hint) {
+    console.log('[breadcrumb]');
+    console.log(breadcrumb);
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    return breadcrumb;
+  },
+  beforeSend(event, hint) {
+    console.log('[event]');
+    console.log(event);
+
+    return event;
+  },
+  beforeSendTransaction(transaction) {
+    console.log('[transaction]');
+    console.log(transaction);
+
+    return transaction;
+  },
   integrations: [
     new Sentry.Replay({
       // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
+      maskAllText: false,
       blockAllMedia: true,
+    }),
+    new Sentry.Integrations.Breadcrumbs({
+      console: false,
     }),
   ],
 });
