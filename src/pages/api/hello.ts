23a9b7d6da01637587fde1c3c5b398dfa8b24a9e
Lio <00700mm@gmail.com>
2024-09-29 17:29:46 +0900

    feat: delay query 추가

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+  delay: number;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { query } = req;
+  const { delay } = query;
+
+  const delayString = String(delay ?? "1000");
+  const delayNumber = Number(delayString);
+
+  await new Promise((resolve) => setTimeout(resolve, delayNumber));
+  res.status(200).json({ name: "John Doe", delay: delayNumber });
 }
