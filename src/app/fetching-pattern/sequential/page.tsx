775b3d339c38625c508d21d31040767f36b43a57
Lio <00700mm@gmail.com>
2024-10-01 17:16:18 +0900

    feat: 순차적 fetching 예시

---
+++ src/app/fetching-pattern/sequential/page.tsx
@@ -0,0 +1,35 @@
+import Post from "@/app/_src/components/Post";
+import { DummyPost } from "@/pages/api/dummy";
+import { HelloData } from "@/pages/api/hello";
+import { Suspense } from "react";
+
+async function SequentialPage() {
+  const fetchA = await fetch("http://localhost:3000/api/hello?delay=3000", {
+    cache: 'no-store'
+  });
+  const fetchAData: HelloData = await fetchA.json();
+
+  return (
+    <div>
+      {fetchAData.name}
+      <Suspense fallback={<div>Loading...</div>}>
+        <ComponentB />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ComponentB() {
+  const data = await fetch("http://localhost:3000/api/dummy");
+  const posts: DummyPost[] = await data.json();
+
+  return (
+    <div className="flex flex-col gap-4 p-20">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
+
+export default SequentialPage;
