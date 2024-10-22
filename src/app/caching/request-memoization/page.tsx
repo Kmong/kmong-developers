e408c857b118b8546d00b30e42e34b835712defb
Lio <00700mm@gmail.com>
2024-10-22 21:15:45 +0900

    feat: 추가

--- src/app/caching/request-memoization/page.tsx
+++ src/app/caching/request-memoization/page.tsx
@@ -2,7 +2,7 @@ import Post from "@/app/_src/components/Post";
 import { DummyPost } from "@/app/api/dummy/route";
 import { HelloData } from "@/app/api/hello/route";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const posts = await getPosts();
@@ -15,13 +15,24 @@ export default async function Page() {
       <Hello />
       <Hello />
       <Hello />
+      <Hello2 />
     </div>
   );
 }
 
 async function Hello() {
   const hello = await getHello();
-  console.log('hello');
+
+  return (
+    <div>
+      {hello.name}
+      <Hello2 />
+    </div>
+  );
+}
+
+async function Hello2() {
+  const hello = await getHello();
 
   return <div>{hello.name}</div>;
 }
@@ -36,6 +47,8 @@ const getPosts = async () => {
 const getHello = async () => {
   const data = await fetch("http://localhost:3000/api/hello?delay=3000");
   const hello: HelloData = await data.json();
+
+  console.log("getHello");
 
   return hello;
 };
